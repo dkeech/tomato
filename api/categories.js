b0622f6fc72ae5c32652f3d76ff9199f3976a1ab
lunitin <dinsmorc@oregonstate.edu>
2020-07-24 14:16:28 -0700

    Added server.js as well as the api routes for users, tasks and categories

---
+++ api/categories.js
@@ -0,0 +1,173 @@
+/* @categories.js
+ *
+ * Routes for /categories
+ *
+ */
+const router = require('express').Router();
+const {
+  getDB
+} = require('../lib/db');
+const TomatoError = require("../lib/tomato-error");
+
+const {
+  genAuthToken,
+  requireAuth
+} = require('../lib/auth');
+
+
+/*
+ * Get all Categories for a user
+ *
+ */
+router.get('/', (req, res, next) => {
+  const db = getDB();
+
+  // Fetch ID from JWT token
+  let user_id = 1;
+  try {
+
+    console.log(" == getCategories: ", user_id);
+
+    let sql = 'SELECT * from categories WHERE user_id = ?';
+
+    db.query(sql, user_id, function(err, results) {
+      if (err) {
+        // Pass any database errors to the error route
+        next(new TomatoError("Database error: " + err.message, 500));
+      } else {
+        // Check for result not found
+
+        // if results
+        console.log('results', results);
+        res.status(200).send(results);
+      }
+    });
+
+  } catch (err) {
+    console.log('== route err catch');
+    // Throw for all errors including DB issues
+    next(new TomatoError("Internal error adding user: " + err.message, 500));
+  }
+
+});
+
+
+/*
+ * Create a new Category
+ *
+ */
+router.post('/', async (req, res, next) => {
+  const db = getDB();
+  //  Validate required fields here
+
+  // Fetch ID from JWT token
+  let user_id = 1;
+
+  if (true) {
+    try {
+
+      console.log(" == insertCategory: ", req.body);
+
+      let sql = 'INSERT INTO categories (user_id, category_name) VALUES (?, ?)';
+
+      let cat = [
+        user_id,
+        req.body.category_name
+      ];
+
+
+      console.log("== cat", cat);
+      db.query(sql, cat, function(err, results) {
+        if (err) {
+          // Pass any database errors to the error route
+          next(new TomatoError("Database error: " + err.message, 500));
+        } else {
+          console.log("== results ", results);
+          res.status(201).send({
+            id: results.insertId
+          });
+        }
+      });
+
+    } catch (err) {
+      console.log('== route err catch');
+      // Throw for all errors including DB issues
+      next(new TomatoError("Internal error adding category: " + err.message, 500));
+    }
+
+  } else {
+    next(new TomatoError("Request is not valid", 400));
+  }
+});
+
+
+/*
+ * Update a Category
+ *
+ */
+router.patch('/:id', async (req, res, next) => {
+  const db = getDB();
+  // Fetch User Id from JWT token
+  user_id = 1;
+
+  // Check here if field set matches
+  try {
+
+    console.log(" == updateCategory: ", req.body);
+
+    let sql = 'UPDATE categories SET ? WHERE category_id = ? AND user_id = ?';
+
+    db.query(sql, [req.body, req.params.id, user_id], function(err, results) {
+      if (err) {
+        // Pass any database errors to the error route
+        next(new TomatoError("Database error: " + err.message, 500));
+      } else {
+          console.log('results', results );
+          res.status(200).send({});
+
+      }
+    });
+
+  } catch (err) {
+    console.log('== route err catch');
+    // Throw for all errors including DB issues
+    next(new TomatoError("Internal error adding user: " + err.message, 500));
+  }
+
+});
+
+
+
+/*
+ * Delete a Category
+ *
+ */
+router.delete('/:id', async (req, res, next) => {
+  const db = getDB();
+  // Fetch user ID from JWT token
+  user_id = 1;
+
+  try {
+
+    console.log(" == deleteCategory: ", req.params.id);
+
+    // ON CASCADE will delete tasks when categories are deleted
+    let sql = 'DELETE from categories WHERE category_id = ? AND user_id = ?;';
+
+    db.query(sql, [req.params.id, user_id], function(err, results) {
+      if (err) {
+        // Pass any database errors to the error route
+        next(new TomatoError("Database error: " + err.message, 500));
+      } else {
+        res.status(200).send({});
+      }
+    });
+
+  } catch (err) {
+    console.log('== route err catch');
+    // Throw for all errors including DB issues
+    next(new TomatoError("Internal error adding user: " + err.message, 500));
+  }
+});
+
+module.exports = router;
---
+++ api/users.js
@@ -0,0 +1,232 @@
+/* @users.js
+ *
+ * Routes for /api/users
+ *
+ */
+
+const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const {
+  getDB
+} = require('../lib/db');
+const TomatoError = require("../lib/tomato-error");
+
+const {
+  genAuthToken,
+  requireAuth
+} = require('../lib/auth');
+
+/*
+ * Create a new User account
+ *
+ */
+router.post('/', async (req, res, next) => {
+  const db = getDB();
+  //  Validate required fields here
+  if (true) {
+    try {
+
+      console.log(" == insertNewUser: ", req.body);
+
+      let sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+      const passwordHash = await bcrypt.hash(req.body.password, 8);
+      const user = [
+        req.body.username,
+        req.body.email,
+        passwordHash
+      ];
+
+      console.log("== user", user);
+      db.query(sql, user, function(err, results) {
+        if (err) {
+          // Pass any database errors to the error route
+          next(new TomatoError("Database error: " + err.message, 500));
+        } else {
+          console.log("== results ", results);
+          res.status(201).send({
+            id: results.insertId
+          });
+        }
+      });
+
+    } catch (err) {
+      console.log('== route err catch');
+      // Throw for all errors including DB issues
+      next(new TomatoError("Internal error adding user: " + err.message, 500));
+    }
+
+  } else {
+    next(new TomatoError("Request is not valid", 400));
+  }
+});
+
+/*
+ * Get the details of a User
+ *
+ * @TODO enable authentication with requireAuth pre-route
+ * router.get('/:id', requireAuth, (req, res, next) => {
+ */
+router.get('/:id', (req, res, next) => {
+  const db = getDB();
+
+  // Check here if ID of user matches ID of JWT token
+  //
+  try {
+
+    console.log(" == getUserDetails: ", req.params.id);
+
+    let sql = 'SELECT user_id, username, email from users WHERE user_id = ?';
+
+    db.query(sql, [req.params.id], function(err, results) {
+      if (err) {
+        // Pass any database errors to the error route
+        next(new TomatoError("Database error: " + err.message, 500));
+      } else {
+        // Check for result not found
+
+        // if results
+        console.log('results', results);
+        res.status(200).send(results[0]);
+      }
+    });
+
+  } catch (err) {
+    console.log('== route err catch');
+    // Throw for all errors including DB issues
+    next(new TomatoError("Internal error adding user: " + err.message, 500));
+  }
+
+
+});
+
+
+/*
+ * Update the details of a User
+ *
+ * @TODO enable authentication with requireAuth pre-route
+ * router.patch('/:id', requireAuth, (req, res, next) => {
+ */
+router.patch('/:id', async (req, res, next) => {
+  const db = getDB();
+  // Check here if ID of user matches ID of JWT token
+  // Check here if field set matches
+  try {
+
+    console.log(" == updateUser: ", req.body);
+
+    let sql = 'UPDATE users SET ? WHERE user_id = ?';
+    let user = req.body;
+    // Crypt the password if necessary
+    if (req.body.password) {
+      user.password = await bcrypt.hash(req.body.password, 8);
+    }
+
+
+    db.query(sql, [user, req.params.id], function(err, results) {
+      if (err) {
+        // Pass any database errors to the error route
+        next(new TomatoError("Database error: " + err.message, 500));
+      } else {
+          console.log('results', results );
+          res.status(200).send({});
+
+      }
+    });
+
+  } catch (err) {
+    console.log('== route err catch');
+    // Throw for all errors including DB issues
+    next(new TomatoError("Internal error adding user: " + err.message, 500));
+  }
+
+});
+
+/*
+ * Delete a User account
+ *
+ */
+// router.delete('/:id/reset', async (req, res, next) => {
+//   res.status(200).send({
+//     body: 'delete an account'
+//   });
+// });
+
+
+/*
+ * Reset a User account - Delete all Categories & Tasks
+ *
+ * @TODO enable authentication with requireAuth pre-route
+ * router.delete('/:id/reset', requireAuth, (req, res, next) => {
+ */
+router.delete('/:id/reset', (req, res, next) => {
+  const db = getDB();
+  // Check here if ID of user matches ID of JWT token
+  //
+  try {
+
+    console.log(" == resetUser: ", req.params.id);
+
+    // ON CASCADE will delete tasks when categories are deleted
+    let sql = 'DELETE from categories WHERE user_id = ? ;';
+
+    db.query(sql, req.params.id, function(err, results) {
+      if (err) {
+        // Pass any database errors to the error route
+        next(new TomatoError("Database error: " + err.message, 500));
+      } else {
+        res.status(200).send({});
+      }
+    });
+
+  } catch (err) {
+    console.log('== route err catch');
+    // Throw for all errors including DB issues
+    next(new TomatoError("Internal error adding user: " + err.message, 500));
+  }
+});
+
+
+/*
+ * Attempt to log a user in
+ *
+ */
+router.post('/login', async (req, res, next) => {
+  const db = getDB();
+
+  try {
+    // Fetch user details from the data base
+    user = {
+      id: 1000,
+      username:'fake',
+      email: 'fake@example.org',
+      password: 'password'
+    }
+
+    checkUser = req.body;
+    console.log("== user", user, "checkuser", checkUser);
+
+    // verify password
+    const auth = user && await bcrypt.compare(user.password, checkUser.password)
+    console.log("== pwd cmp", await bcrypt.compare(user.password, checkUser.password))
+    if (auth) {
+
+      // frontend does not need password
+      delete user.password;
+
+      // Generate a JWT token with the user payload
+      const token = genAuthToken(user);
+
+      res.status(200).send({
+        token: token
+      });
+    } else {
+      next(new TomatoError("Authentication failed.", 401));
+    }
+  } catch (err) {
+    console.error(err);
+    next(new TomatoError("Authentication error.  Please try again later.", 500));
+  }
+});
+
+
+module.exports = router;
